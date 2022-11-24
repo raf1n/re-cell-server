c9e6869bfde7d27e166788b234c730f35fc9eea0
raf1n <rafinc10@gmail.com>
2022-11-24 14:47:08 +0600

    categories-get-endpoint-added

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 const app = express();
+
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -19,6 +20,24 @@ const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
+});
+
+// collections
+const categoriesCollection = client.db("reCell").collection("categories");
+const productsCollection = client.db("reCell").collection("products");
+
+// products category
+
+app.get("/categories", async (req, res) => {
+  try {
+    const result = await categoriesCollection.find({}).toArray();
+    res.send({
+      status: "success",
+      data: result,
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 });
 
 app.listen(port, () => {
